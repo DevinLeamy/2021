3cb6fb5acd902854eb1f58ccb598b9cda7c42e67
Devin Leamy <devinleamy@gmail.com>
2021-06-23 19:08:38 -0600

    added light/dark mode toggle

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,17 +1,36 @@
 import './App.css';
+import { useState, useEffect } from "react";
 import { Splash } from '../splash/splash';
 import { About } from "../about/about";
 import { Projects } from "../projects/projects";
 import { Contact } from "../contact/contact";
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import Brightness2Icon from '@material-ui/icons/Brightness2';
+
+// Themes
+const DARK = "rgb(29, 29, 29)";
+const LIGHT = "rgb(252, 252, 252)";
 
 function App() {
-	// document.body.style.color = "rgb(29, 29, 29)"
-	// document.body.style.backgroundColor = "rgb(252, 252, 252)"
+	const [darkMode, setDarkMode] = useState(true);
+	useEffect(() => {
+		// Set theme
+		document.body.style.color = (darkMode)? LIGHT : DARK;
+		document.body.style.backgroundColor = (darkMode)? DARK : LIGHT;
+	}, [darkMode])
 	return (
-		<div> 
-			<div>
-				<Splash></Splash>
-			</div>
+		<div className="main-container"> 
+			{!darkMode && 
+				<div className="icon-wrapper" onClick={() => setDarkMode(true)}>
+					<WbSunnyIcon style={{fontSize: "40px", color: DARK}} />
+				</div>
+			}
+			{darkMode && 
+				<div className="icon-wrapper" onClick={() => setDarkMode(false)}>
+					<Brightness2Icon style={{fontSize: "40px", color: LIGHT}} />
+				</div>
+			}
+			<Splash></Splash>
 			<About></About>
 			<Projects></Projects>
 			<Contact></Contact>
